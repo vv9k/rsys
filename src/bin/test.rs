f45412bd9213d6166389c88616d4a2495b11202b
Wojciech <wojciech@wkepka.dev>
2020-08-31 19:14:40 +0200

    Update test binary

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,8 +1,7 @@
 #[allow(unused_imports)]
 use rsys::*;
-use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Error> {
     println!("{}", rsys::hostname()?);
     let iface = rsys::default_iface()?;
     println!("{}", rsys::ipv4(&iface)?);
