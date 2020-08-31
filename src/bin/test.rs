fd59631acf723e06a4828e649daeeccdf6a29c8b
Wojciech Kępka <wojtek.kepka@protonmail.com>
2020-08-31 20:01:10 +0200

    Update test binary

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -2,18 +2,22 @@
 use rsys::*;
 
 fn main() -> Result<(), Error> {
-    println!("{}", rsys::hostname()?);
+    println!("HOSTNAME - {}", rsys::hostname()?);
     let iface = rsys::default_iface()?;
-    println!("{}", rsys::ipv4(&iface)?);
-    println!("{}", rsys::mac(&iface)?);
-    println!("{}", rsys::cpu()?);
-    println!("{}", rsys::arch()?);
-    println!("{}", rsys::memory()?);
-    println!("{}", rsys::uptime()?);
-    println!("{:#?}", rsys::interfaces()?);
-    println!("{}", rsys::swap()?);
-    println!("{}", rsys::cpu_cores()?);
-    println!("{}", rsys::cpu_clock()?);
-    println!("{}", rsys::linux::kernel_version()?);
+    println!("CPU - {}", rsys::cpu()?);
+    println!("ARCH - {}", rsys::arch()?);
+    println!("MEMORY - {} b", rsys::memory()?);
+    println!("UPTIME - {} s", rsys::uptime()?);
+    println!("SWAP - {}b", rsys::swap()?);
+    println!("CPU CORES - {}", rsys::cpu_cores()?);
+    println!("CPU CLOCK - {} MHz", rsys::cpu_clock()?);
+    println!("IPv4 - {}", rsys::ipv4(&iface)?);
+    if cfg!(target_os = "linux") {
+        println!("MAC - {}", rsys::mac(&iface)?);
+        println!("KERNEL VERSION - {}", rsys::linux::kernel_version()?);
+        println!("INTERFACES - {:#?}", rsys::interfaces()?);
+    } else if cfg!(target_os = "macos") {
+        println!("{}", rsys::macos::model()?);
+    }
     Ok(())
 }
