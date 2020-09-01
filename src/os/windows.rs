4a710c8c18c68380242282ef774fc24230eef542
Krzysztof Karpieszuk <krzysztof.karpieszuk@gmail.com>
2020-09-01 21:18:58 +0200

    Make most of windows api work

--- src/os/windows.rs
+++ src/os/windows.rs
@@ -1,117 +1,176 @@
 use super::Error;
+use std::ffi::OsString;
 use std::mem;
+use std::os::windows::ffi::OsStringExt;
+use std::ptr::{null, null_mut};
 use winapi::{
-    shared::ntdef::{LARGE_INTEGER, NULL},
+    shared::{
+        minwindef::{HKEY, MAX_PATH},
+        winerror::ERROR_SUCCESS,
+    },
     um::{
         errhandlingapi::GetLastError,
         lmwksta::{NetWkstaGetInfo, WKSTA_INFO_100},
-        profileapi::QueryPerformanceFrequency,
         sysinfoapi::{
-            GetLogicalProcessorInformation, GetPhysicallyInstalledSystemMemory, GetSystemInfo, GetTickCount64,
-            GlobalMemoryStatusEx, SYSTEM_INFO_u, MEMORYSTATUSEX, SYSTEM_INFO,
+            GetLogicalProcessorInformation, GetProcessorSystemCycleTime, GetSystemInfo, GetTickCount64,
+            GlobalMemoryStatusEx, MEMORYSTATUSEX, SYSTEM_INFO,
         },
-        winbase::{
-            FormatMessageA, GetComputerNameA, FORMAT_MESSAGE_ALLOCATE_BUFFER, FORMAT_MESSAGE_FROM_SYSTEM,
-            FORMAT_MESSAGE_IGNORE_INSERTS,
+        winbase::{FormatMessageW, GetComputerNameW, FORMAT_MESSAGE_FROM_SYSTEM, FORMAT_MESSAGE_IGNORE_INSERTS},
+        winnt::{
+            KEY_READ, LANG_NEUTRAL, MAKELANGID, SUBLANG_DEFAULT, SYSTEM_LOGICAL_PROCESSOR_INFORMATION,
+            SYSTEM_PROCESSOR_CYCLE_TIME_INFORMATION,
         },
-        winnt::{LANG_NEUTRAL, MAKELANGID, SUBLANG_DEFAULT, SYSTEM_LOGICAL_PROCESSOR_INFORMATION},
+        winreg::{RegOpenKeyExW, RegQueryValueExW, HKEY_LOCAL_MACHINE},
     },
 };
 
 //################################################################################
 // INTERNAL
 
+const BUF_SIZE: usize = 4096;
+const NUL: char = '\0';
+const CARIAGE: char = '\r';
+const NL: char = '\n';
+
+//https://github.com/retep998/winapi-rs/issues/780
+const MAX_COMPUTERNAME_LENGTH: u32 = 31;
+
 fn last_error() -> u32 {
     unsafe { GetLastError() }
 }
 
-fn utf8_buf_to_string(buf: &[i8]) -> Result<String, Error> {
-    Ok(std::str::from_utf8(&buf.iter().map(|b| *b as u8).collect::<Vec<u8>>())
-        .map_err(|e| Error::WinApiError(format!("got invalid utf8 string - `{}`", e)))?
+fn utf16_buf_to_string(buf: &[u16]) -> Result<String, Error> {
+    Ok(OsString::from_wide(&buf)
+        .to_string_lossy()
+        .to_string()
+        .trim_end_matches(NUL)
+        .trim_end_matches(NL)
+        .trim_end_matches(CARIAGE)
         .to_string())
 }
 
-fn last_error_msg() -> Result<String, Error> {
-    let mut out_buf: Vec<u8> = Vec::new();
-    let mut out_size: u32;
-
+fn last_error_msg() -> Result<(u32, String), Error> {
+    let mut out_buf: Vec<u16> = vec![0; BUF_SIZE];
+    let mut last_id = 0;
     unsafe {
-        let last_id = last_error();
-        FormatMessageA(
-            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-            NULL,
+        last_id = last_error();
+
+        FormatMessageW(
+            FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+            null(),
             last_id,
-            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT) as u32,
             out_buf.as_mut_ptr(),
-            out_size as *mut u32,
-            NULL,
-        )
+            BUF_SIZE as u32,
+            null_mut(),
+        );
     }
 
-    utf8_buf_to_string(&out_buf)
+    utf16_buf_to_string(&out_buf).map(|s| (last_id, s))
 }
 
 fn system_info() -> SYSTEM_INFO {
     unsafe {
         let mut info: SYSTEM_INFO = mem::zeroed();
-        GetSystemInfo(info.as_mut_ptr());
+        GetSystemInfo(&mut info);
         info
     }
 }
 
 fn memory_status() -> Result<MEMORYSTATUSEX, Error> {
     unsafe {
-        let mut info: MEMORYSTATUSEX = mem::zeroed();
-        let is_success = GlobalMemoryStatusEx(info.as_mut_ptr()) as bool;
+        let mut info = mem::zeroed::<MEMORYSTATUSEX>();
+        info.dwLength = mem::size_of::<MEMORYSTATUSEX>() as u32;
+        let is_success = GlobalMemoryStatusEx(&mut info) != 0;
         if !is_success {
-            return Err(Error::WinApiError(last_error_msg()?));
+            let (id, msg) = last_error_msg()?;
+            return Err(Error::WinApiError(id, msg));
         }
         Ok(info)
     }
 }
 
-fn net_wksta() -> WKSTA_INFO_100 {
-    unsafe {
-        let mut info: WKSTA_INFO_100 = mem::zeroed();
-        // Check for error here
-        NetWkstaGetInfo(NULL as *mut u16, 100, info.as_mut_ptr());
-        info
-    }
-}
+// fn net_wksta() -> WKSTA_INFO_100 {
+//     unsafe {
+//         let mut info: WKSTA_INFO_100 = mem::zeroed();
+//         // Check for error here
+//         NetWkstaGetInfo(NULL as *mut u16, 100, &mut info);
+//         info
+//     }
+// }
 
-fn logical_processor_information() -> Result<SYSTEM_LOGICAL_PROCESSOR_INFORMATION, Error> {
+fn logical_processor_information() -> Result<Vec<SYSTEM_LOGICAL_PROCESSOR_INFORMATION>, Error> {
     unsafe {
-        let mut info: SYSTEM_LOGICAL_PROCESSOR_INFORMATION = mem::zeroed();
-        let mut info_size: u32;
-        let is_success = GetLogicalProcessorInformation(info.as_mut_ptr(), info_size as *mut u32);
+        let x = mem::zeroed::<SYSTEM_LOGICAL_PROCESSOR_INFORMATION>();
+        let mut info = vec![x; BUF_SIZE];
+        let mut ret_length: u32 = BUF_SIZE as u32;
+        let is_success = GetLogicalProcessorInformation(info.as_mut_ptr(), &mut ret_length) != 0;
         if !is_success {
-            return Err(Error::WinApiError(last_error_msg()?));
+            let (id, msg) = last_error_msg()?;
+            return Err(Error::WinApiError(id, msg));
         }
         Ok(info)
     }
 }
 
 fn is_cpu_hyperthreaded() -> Result<bool, Error> {
-    unsafe { Ok(logical_processor_information()?.u.ProcessorCore().Flags == 1) }
+    unsafe { Ok(logical_processor_information()?[0].u.ProcessorCore().Flags == 1) }
 }
 
+#[allow(dead_code)]
 fn pagesize() -> u32 {
     system_info().dwPageSize
+}
+
+fn reg_lm_val<T>(subkey: &str, val: &str) -> Result<T, Error> {
+    unsafe {
+        let mut hkey = mem::zeroed::<HKEY>();
+        let mut subkey = subkey.encode_utf16().collect::<Vec<u16>>();
+        println!("hkey = `{:?}`", hkey);
+        let mut is_success =
+            RegOpenKeyExW(HKEY_LOCAL_MACHINE, subkey.as_ptr(), 0, KEY_READ, &mut hkey) as u32 != ERROR_SUCCESS;
+        if !is_success {
+            let (id, msg) = last_error_msg()?;
+            return Err(Error::WinApiError(id, msg));
+        }
+        let mut buf_size = mem::size_of::<T>();
+        let mut dbuf = vec![0u8; buf_size];
+        let mut val = val.encode_utf16().collect::<Vec<u16>>();
+        let mut tbuf = vec![0u32; buf_size];
+        let mut null = 0;
+        is_success = RegQueryValueExW(
+            hkey,
+            val.as_ptr(),
+            null as *mut u32,
+            tbuf.as_mut_ptr(),
+            dbuf.as_mut_ptr(),
+            buf_size as *mut u32,
+        ) as u32
+            != ERROR_SUCCESS;
+        if !is_success {
+            let (id, msg) = last_error_msg()?;
+            return Err(Error::WinApiError(id, msg));
+        }
+        println!("dbuf = `{:?}`", &dbuf);
+        println!("tbuf = `{:?}`", &tbuf);
+        Ok(mem::transmute_copy::<Vec<u8>, T>(&dbuf))
+    }
 }
 
 //################################################################################
 // PUBLIC
 
 pub(crate) fn _hostname() -> Result<String, Error> {
-    let mut out_buf: Vec<i8> = Vec::new();
-    let mut out_size: u32;
+    let mut out_buf: Vec<u16> = vec![0; BUF_SIZE];
+    let mut out_size: u32 = MAX_COMPUTERNAME_LENGTH;
     unsafe {
-        let is_success = GetComputerNameA(out_buf.as_mut_ptr(), out_size as *mut u32) as bool;
-        if !is_success {
-            return Err(Error::WinApiError(last_error_msg()?));
+        let ret = GetComputerNameW(out_buf.as_mut_ptr(), &mut out_size);
+        if ret == 0 {
+            let (id, msg) = last_error_msg()?;
+            return Err(Error::WinApiError(id, msg));
         }
     }
-    utf8_buf_to_string(&out_buf)
+    utf16_buf_to_string(&out_buf)
 }
 
 pub(crate) fn _uptime() -> Result<u64, Error> {
@@ -136,15 +195,10 @@ pub(crate) fn _cpu() -> Result<String, Error> {
     todo!()
 }
 
+// # TODO
+// Figure out why the registry is returning an empty buffer (probably not finding the right hkey?)
 pub(crate) fn _cpu_clock() -> Result<f32, Error> {
-    unsafe {
-        let mut freq: LARGE_INTEGER = mem::zeroed();
-        let is_success = QueryPerformanceFrequency(freq.as_mut_ptr()) as bool;
-        if !is_success {
-            return Err(Error::WinApiError(last_error_msg()?));
-        }
-        Ok(freq.QuadPart() as f32)
-    }
+    reg_lm_val::<u32>("HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", "~MHz").map(|v| v as f32)
 }
 
 pub(crate) fn _cpu_cores() -> Result<u16, Error> {
@@ -156,7 +210,7 @@ pub(crate) fn _cpu_cores() -> Result<u16, Error> {
 }
 
 pub(crate) fn _logical_cores() -> Result<u16, Error> {
-    system_info().dwNumberOfProcessors
+    Ok(system_info().dwNumberOfProcessors as u16)
 }
 
 pub(crate) fn _memory() -> Result<usize, Error> {
@@ -188,5 +242,6 @@ pub(crate) fn _interfaces() -> Result<Vec<String>, Error> {
 }
 
 pub(crate) fn _domainname() -> Result<String, Error> {
-    Ok(net_wksta().wki100_langroup)
+    // Ok(net_wksta().wki100_langroup)
+    todo!()
 }
