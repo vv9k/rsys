85edf3e6222d9a51d2aa4062b9f4a917c8751253
Wojciech <wojciech@wkepka.dev>
2020-09-10 20:51:35 +0200

    Add PartialEq and Eq to RsysError

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
 use thiserror::Error;
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, PartialEq, Eq)]
 pub enum RsysError {
     #[error("Failed to execute command - `{0}`")]
     CommandRunError(String),
