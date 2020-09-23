919024f93b45646784f089c06793932e522985db
wojciechkepka <wojtek.kepka@protonmail.com>
2020-09-23 19:53:37 +0200

    Update test binary

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,40 +1,31 @@
 #[allow(unused_imports)]
-use rsys::{Error, Rsys};
+use rsys::{Result, Rsys};
 
-fn display() -> Result<(), Error> {
+fn main() -> Result<()> {
+    // You can either use api through Rsys object
+    // for os-agnostic experience
     let rsys = Rsys::new();
     println!("HOSTNAME - {}", rsys.hostname()?);
     let iface = rsys.default_iface()?;
     println!("CPU - {}", rsys.cpu()?);
     println!("ARCH - {}", rsys.arch()?);
-    println!("TOTAL MEMORY - {} b", rsys.memory_total()?);
-    println!("FREE MEMORY - {} b", rsys.memory_free()?);
-    println!("UPTIME - {} s", rsys.uptime()?);
-    println!("TOTAL SWAP - {}b", rsys.swap_total()?);
-    println!("FREE SWAP - {}b", rsys.swap_total()?);
+    println!("MEMORY TOTAL - {}b", rsys.memory_total()?);
+    println!("UPTIME - {}s", rsys.uptime()?);
+    println!("SWAP TOTAL - {}b", rsys.swap_total()?);
     println!("CPU CORES - {}", rsys.cpu_cores()?);
-    println!("LOGICAL CORES - {}", rsys.logical_cores()?);
-    println!("CPU CLOCK - {} MHz", rsys.cpu_clock()?);
+    println!("CPU CLOCK - {}MHz", rsys.cpu_clock()?);
     println!("IPv4 - {}", rsys.ipv4(&iface)?);
     println!("MAC - {}", rsys.mac(&iface)?);
     println!("INTERFACES - {:#?}", rsys.interfaces()?);
 
-    // On linux
-    //println!("MOUNTS - {:?}", rsys::linux::mounts()?);
-    //println!("IFACES - {:?}", rsys::linux::ifaces()?);
-    //println!("PIDS - {:?}", rsys::linux::pids()?);
-    //println!("PROCESSES - {:#?}", rsys::linux::processes()?);
-    //println!("SDA - {:#?}", rsys::linux::stat_block_device("sda")?);
-    //println!("DM0 - {:#?}", rsys::linux::stat_device_mapper("dm-0")?);
-    //println!("DM1 - {:#?}", rsys::linux::stat_device_mapper("dm-1")?);
-    //println!("DM2 - {:#?}", rsys::linux::stat_device_mapper("dm-2")?);
+    // Or use functions in each module
+    if cfg!(target_os = "linux") {
+        println!("KERNEL VERSION - {}", rsys::linux::kernel_version()?);
+        println!("HOSTNAME - {}", rsys::linux::hostname()?);
 
-    Ok(())
-}
-
-fn main() -> Result<(), Error> {
-    if let Err(e) = display() {
-        println!("{}", e);
+        // Os-specific functions are also available as methods
+        println!("MEMORY - {:#?}", rsys.memory()?);
+        println!("KERNEL_VERSION - {}", rsys.kernel_version()?);
     }
     Ok(())
 }
