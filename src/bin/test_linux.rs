a83618c1b4c8ad381c76d022dcbec8c04829e42a
wojciechkepka <wojtek.kepka@protonmail.com>
2020-09-23 21:35:16 +0200

    Fix test binary

--- src/bin/test_linux.rs
+++ src/bin/test_linux.rs
@@ -1,12 +1,17 @@
-#![cfg(target_os = "linux")]
 #[allow(unused_imports)]
 use rsys::{Result, Rsys};
 
+#[cfg(target_os = "linux")]
 fn main() -> Result<()> {
     let rsys = Rsys::new();
     println!("KERNEL VERSION - {}", rsys::linux::kernel_version()?);
     println!("HOSTNAME - {}", rsys::linux::hostname()?);
     println!("MEMORY - {:#?}", rsys.memory()?);
     println!("KERNEL_VERSION - {}", rsys.kernel_version()?);
+    Ok(())
+}
+
+#[cfg(not(target_os = "linux"))]
+fn main() -> Result<()> {
     Ok(())
 }
