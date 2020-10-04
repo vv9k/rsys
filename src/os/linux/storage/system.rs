6826857dfdafc44f3b71d30c470ff5adfce8be8e
wojciechkepka <wojtek.kepka@protonmail.com>
2020-10-04 09:37:13 +0200

    [linux] map blocksize as i64

--- src/os/linux/storage/system.rs
+++ src/os/linux/storage/system.rs
@@ -4,5 +4,5 @@ use nix::sys::statfs::statfs;
 pub(crate) fn blk_bsz_get(path: &str) -> Result<i64> {
     statfs(path)
         .map_err(|e| Error::FfiError(format!("getting block size of `{}`", path), e.to_string()))
-        .map(|v| v.block_size())
+        .map(|v| v.block_size() as i64)
 }
