de9ce1962d42b50b17505c4d57276ed47568aace
Wojciech Kępka <wojciech@wkepka.dev>
2021-10-29 20:03:45 +0200

    Use a single test binary

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,3 +1,4 @@
+#[allow(unused_imports)]
 use rsys::{Result, Rsys};
 
 fn main() -> Result<()> {
@@ -14,5 +15,14 @@ fn main() -> Result<()> {
     println!("SWAP FREE - {}b", rsys.swap_free()?);
     println!("MEMORY TOTAL - {}b", rsys.memory_total()?);
     println!("MEMORY FREE - {}b", rsys.memory_free()?);
+
+    #[cfg(target_os = "linux")]
+    {
+        println!("KERNEL VERSION - {}", rsys::linux::kernel_release()?);
+        println!("HOSTNAME - {}", rsys::linux::hostname()?);
+        println!("KERNEL_VERSION - {}", rsys.kernel_release()?);
+        println!("{:#?}", rsys::linux::cpu::processor()?);
+        println!("MOUNTS - {:#?}", rsys::linux::mounts::mounts()?);
+    }
     Ok(())
 }
